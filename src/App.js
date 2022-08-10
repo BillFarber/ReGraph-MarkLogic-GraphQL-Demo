5023b39185d1decc6feac9034c78b4cc82fd60fb
Phil Barber <Phil.Barber@MarkLogic.com>
2022-08-10 18:30:55 -0400

    Cleanup the project just a bit.
    More cleanup
    
    
    More cleanup
    
    
    
    Former-commit-id: 901a9c8449cf8016482d8dfbc9fc27166089f6c1
    Former-commit-id: 831ebe5a74c0efb0b0239454e9d82d1d757763d6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React  from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import { Chart } from "regraph";
+import {Chart} from "regraph";
+
 const axios = require('axios');
 
 //const url = "http://127.0.0.1:8004/LATEST/resources/graphql?rs:query=query%20someQuery%20%7B%20graphql_Humans%20%7B%20id%20name%20%7D%20%7D";
@@ -9,14 +10,14 @@ const url = `http://127.0.0.1:8004/LATEST/resources/graphql?rs:query=${graphqlQu
 
 function createHumanNode(name) {
   return {
-    label: { text: name },
+    label: {text: name},
     color: "#0066ff",
   };
 }
 
 function createCarNode(model) {
   return {
-    label: { text: model },
+    label: {text: model},
     color: "#39e600",
   };
 }
@@ -57,35 +58,35 @@ function getHumanData_axios() {
       headers: {
         'Content-Type': 'application/graphql'
       }
-  })
+    })
     .then((response) => {
       // A little hard-coded data that can be used for testing & development
       //  const data = {"graphql_Humans":[{"id":1001, "name":"Jenny", "friends":[{"id":2},{"id":1002},{"id":1000}]}, {"id":1000, "name":"Jane", "friends":[]}, {"id":2, "name":"Jim", "friends":[]}, {"id":3, "name":"Joe", "friends":[]}, {"id":1002, "name":"Joan", "friends":[{"id":3}, {"id":1}]}, {"id":1, "name":"John", "friends":[]}]}
-       const items = toReGraphFormat(response.data.data);
-       //const items = toReGraphFormat(data);
-       ReactDOM.render(
-         <div style={{ width: "100vw", height: "100vh" }}>
-           <Chart items={items} />
-         </div>,
-         document.getElementById("root")
-       );
-     })
-     .catch((error) => {
-       console.error(error);
-       const data = {"graphql_Humans":[{"id":1, "name": JSON.stringify(error)}]}
-       const items = toReGraphFormat(data);
-       ReactDOM.render(
-         <div style={{ width: "100vw", height: "100vh" }}>
-           <Chart items={items} />
-         </div>,
-         document.getElementById("root")
-       );
-     });
+      const items = toReGraphFormat(response.data.data);
+      //const items = toReGraphFormat(data);
+      ReactDOM.render(
+        <div style={{width: "100vw", height: "100vh"}}>
+          <Chart items={items}/>
+        </div>,
+        document.getElementById("root")
+      );
+    })
+    .catch((error) => {
+      console.error(error);
+      const data = {"graphql_Humans": [{"id": 1, "name": JSON.stringify(error)}]}
+      const items = toReGraphFormat(data);
+      ReactDOM.render(
+        <div style={{width: "100vw", height: "100vh"}}>
+          <Chart items={items}/>
+        </div>,
+        document.getElementById("root")
+      );
+    });
 }
 
 const DEFAULT_ITEMS = {
-  [`hashtag_AA`]: {
-    label: { text: "BB" }
+  [`hashtag_Loading`]: {
+    label: {text: "Loading"}
   }
 };
 
@@ -94,8 +95,8 @@ function App() {
   getHumanData_axios();
 
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Chart items={items} />
+    <div style={{width: "100vw", height: "100vh"}}>
+      <Chart items={items}/>
     </div>
   );
 }
